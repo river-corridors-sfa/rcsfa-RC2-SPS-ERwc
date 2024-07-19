5c31e6d074e87c04868ddbf2772674308b7682c4
mmlaan <maggi.laan@gmail.com>
2024-07-19 10:55:43 -0700

    Create ERwc_calculation.R

---
+++ Code/ERwc_calculation/ERwc_calculation.R
@@ -0,0 +1,117 @@
+# RC2 spatial study 2021
+# calculate ERwc
+# M Laan July 19 2024
+rm(list=ls(all=TRUE))
+
+library(xlsx)
+library(tidyverse)
+library(reshape)
+library(ggpmisc)
+
+DO_Path = 'C:/GitHub/rcsfa-RC2-SPS-ERwc/Data/Multiple_linear_regression/v3_Minidot_Summary_Statistics.csv'
+
+Out_Path = 'C:/GitHub/rcsfa-RC2-SPS-ERwc/Data/Multiple_linear_regression/'
+
+#sdir<- 'C:/Brieanne/GitHub/SSS_metabolism/'
+
+
+##########################################################################
+## DO slope
+
+# Read in miniDOT manual chamber summary file
+DO_summary<-read_csv(DO_Path, comment = '#', na = c('N/A', -9999))
+
+
+# Pull out slope/NRMSE from each site 
+DO_slope<-DO_summary %>%
+  select(Site_ID,Dissolved_Oxygen_1_Slope,
+                              Dissolved_Oxygen_2_Slope,Dissolved_Oxygen_3_Slope,
+                              Dissolved_Oxygen_1_NRMSE,Dissolved_Oxygen_2_NRMSE,Dissolved_Oxygen_3_NRMSE, Dissolved_Oxygen_1_RMSE, Dissolved_Oxygen_2_RMSE, Dissolved_Oxygen_3_RMSE, Dissolved_Oxygen_1_Rsquared, Dissolved_Oxygen_2_Rsquared, Dissolved_Oxygen_3_Rsquared, Temperature_1_Mean, Temperature_2_Mean, Temperature_3_Mean)
+
+# If NRMSE > 0.03, remove slope (largest is 0.003 in this dataset, which is a 0.3% error)
+
+for (i in c(1:3)){
+  k = which(DO_slope[,paste0('Dissolved_Oxygen_',i,'_NRMSE')]>0.03)
+  DO_slope[k,paste0('Dissolved_Oxygen_',i,'_Slope')]<-NA
+}
+
+# No depth measurements, so will stay as mg O2/L/min
+
+# Look at other possible NRMSE issues 
+# df_melt <- reshape2::melt(DO_slope, id.vars = "Site_ID")
+# 
+# df_melt <- df_melt %>%
+#     mutate(Rep = sub("Dissolved_Oxygen_(\\d)_(.*)", "\\1", variable),
+#            Measure = sub("Dissolved_Oxygen_(\\d)_(.*)", "\\2", variable)) %>%
+#     select(-variable) %>%
+#     pivot_wider(names_from = Measure, values_from = value)
+# 
+# slope_hist <- ggplot(data = df_melt, aes(x = Slope ))+
+#     geom_histogram()
+# 
+# nrmse_hist <- ggplot(data = df_melt, aes(x = NRMSE ))+
+#     geom_histogram()
+# 
+# r2_hist <- ggplot(data = df_melt, aes(x = Rsquared ))+
+#     geom_histogram()
+# 
+# 
+# scatter_slope_r2 <- ggplot(data = df_melt, aes(x = log10(abs(Slope)), y = Rsquared)) +
+#     geom_point() +
+#     geom_smooth(method = "lm", formula = y ~ x)+
+#     stat_poly_eq(aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+#                  formula = y ~ x, parse = TRUE, size = 5)
+# 
+# scatter_slope_nrmse_log <- ggplot(data = df_melt, aes(x = log10(abs(Slope)), y = NRMSE)) +
+#     geom_point() +
+#     geom_smooth(method = "lm", formula = y ~ x)+
+#     stat_poly_eq(aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+#                  formula = y ~ x, parse = TRUE, size = 5)
+# 
+#   scatter_slope_nrmse <- ggplot(data = df_melt, aes(x = Slope, y = NRMSE)) +
+#     geom_point() +
+#     geom_smooth(method = "lm", formula = y ~ x)+
+#     stat_poly_eq(aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+#                  formula = y ~ x, parse = TRUE, size = 5)
+# 
+# scatter_nrmse_r2 <- ggplot(data = df_melt, aes(x = Rsquared, y = NRMSE)) +
+#   geom_point() +
+#   geom_smooth(method = "lm", formula = y ~ x)+
+#   stat_poly_eq(aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+#                formula = y ~ x, parse = TRUE, size = 5)
+
+## Convert to mg O2/L/day
+
+ERwc <- DO_slope %>%
+  mutate(Water_Column_Respiration_1 = signif(Dissolved_Oxygen_1_Slope*(60*24), 3),
+         Water_Column_Respiration_2 = signif(Dissolved_Oxygen_2_Slope*(60*24), 3),
+         Water_Column_Respiration_3 = signif(Dissolved_Oxygen_3_Slope*(60*24), 3)) %>%
+  select(-contains('Slope')) %>% 
+  rowwise() %>% 
+  mutate(Mean_ERwc = mean(c(Water_Column_Respiration_1, Water_Column_Respiration_2, Water_Column_Respiration_3), na.rm = TRUE)) %>% 
+  mutate(SD_ERwc = sd(c(Water_Column_Respiration_1, Water_Column_Respiration_2, Water_Column_Respiration_3), na.rm = TRUE)) %>% 
+  mutate(CV_ERwc = (SD_ERwc/Mean_ERwc)*100) %>% 
+  mutate(Mean_Temp = mean(c(Temperature_1_Mean, Temperature_2_Mean, Temperature_3_Mean), na.rm = TRUE)) %>% 
+  mutate(SD_Temp = sd(c(Temperature_1_Mean, Temperature_2_Mean, Temperature_3_Mean), na.rm = TRUE)) %>% 
+  mutate(CV_Temp = (SD_Temp/Mean_Temp)*100) %>% 
+  relocate(Mean_ERwc, .after = Site_ID) %>% 
+  relocate(CV_ERwc, .after = Mean_ERwc)
+
+ERwc_mean_melt =  reshape2::melt(ERwc, id.vars = "Site_ID") %>% 
+  filter(grepl("Respiration", variable)) 
+                      
+          
+ggplot(ERwc_mean_melt, aes(x = Site_ID, y = value)) + 
+  geom_boxplot()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggplot(ERwc, aes(x = CV_ERwc, y = Mean_ERwc)) + 
+  geom_point(aes(color = Mean_ERwc < -0.5))
+
+## pull out mean temperature
+
+ERwc_mean = ERwc %>% 
+  select(c(Site_ID, Mean_ERwc, Mean_Temp))
+
+write.csv(ERwc_mean, paste0(Out_Path, '/ERwc_Mean.csv'))
+  
