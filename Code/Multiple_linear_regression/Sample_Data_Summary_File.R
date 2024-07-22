b90997679e56711920aed262d45d7d4277a3278d
mmlaan <maggi.laan@gmail.com>
2024-07-22 15:58:11 -0700

    Start summary file for sample data

--- Code/Multiple_linear_regression/Sample_Data_Summary_File.R
+++ Code/Multiple_linear_regression/Sample_Data_Summary_File.R
@@ -7,3 +7,21 @@ setwd(dirname(current_path))
 setwd("../..")
 getwd()
 
+
+## TSS 
+
+tss_path = "./Data/Multiple_linear_regression/SPS_TSS.csv"
+
+tss = read_csv(tss_path, skip = 2) %>% 
+  slice(-1:-11) %>% 
+  select(c("Sample_Name", "00530_TSS_mg_per_L", "Methods_Deviation")) %>% 
+  mutate(Sample_Name = str_replace(Sample_Name, "TSS", "MEAN"))
+
+## DIC 
+
+dic_path = "./Data/Multiple_linear_regression/v2_SPS_DIC.csv"
+
+dic = read_csv(dic_path, skip = 2) %>% 
+  slice(-1:-11, -107) %>% 
+  select(c("Sample_Name", "00530_TSS_mg_per_L", "Methods_Deviation")) %>% 
+  mutate(Sample_Name = str_replace(Sample_Name, "TSS", ""))
