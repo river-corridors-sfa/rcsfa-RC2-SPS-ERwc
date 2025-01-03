0e5fd31303eb74d77720e5c6ba09fda089e9f83a
brieanne-forbes <brieanne.forbes@pnnl.gov>
2025-01-03 15:04:33 -0800

    Create new ERwc LULC map

--- Code/Map_Code/ERwc_Manuscript_Map.R
+++ Code/Map_Code/ERwc_Manuscript_Map.R
@@ -7,7 +7,7 @@
 # ==============================================================================
 #
 # Author: Brieanne Forbes (brieanne.forbes@pnnlgov)
-# 4 August 2023
+# 2 January 2025
 #
 # ==============================================================================
 library(tidyverse) #keep it tidy
@@ -24,6 +24,7 @@ library(spData)
 library(cowplot)
 library(rstudioapi)
 library(viridis)
+library(terra)
 
 rm(list=ls(all=T))
 
@@ -31,18 +32,19 @@ rm(list=ls(all=T))
 current_path <- rstudioapi::getActiveDocumentContext()$path
 setwd(dirname(current_path))
 setwd("../..")
-getwd()
 
 # ================================= User inputs ================================
 
 # download the metadata from https://data.ess-dive.lbl.gov/view/doi:10.15485/1898914 and use the file `v2_SFA_SpatialStudy_2021_SampleData/SPS_Sample_Field_Metadata.csv`
-metadata_file <- './SPS_Sample_Field_Metadata.csv' # replace this path with the path of the file you downloaded
+metadata_file <- './Data/Multiple_linear_regression/v3_SFA_SpatialStudy_2021_SampleData/SPS_Sample_Field_Metadata.csv' # replace this path with the path of the file you downloaded
 
 data_file <- './Data/Multiple_linear_regression/spatial_data.csv'
 
 yrb_shp_dir <- './Data/Map_Layers/Yakima_River_Basin'
 
 cluster_shp_dir <- './Data/Map_Layers/YRB_Cluster'
+
+land_cover_shp_dir <- './Data/Map_Layers/landcover16_yk'
 
 common_crs = 4326
 
@@ -74,24 +76,24 @@ sites <- st_as_sf(merge, coords = c('Longitude','Latitude'), crs = common_crs)
 
 YRB_flowlines <- get_nhdplus(AOI = YRB_boundary$geometry, streamorder = 3)
 
-elevation_raw <- get_elev_raster(YRB_boundary$geometry, z = 10)
+# elevation_raw <- get_elev_raster(YRB_boundary$geometry, z = 10)
+# 
+# elevation_crop <- mask(elevation_raw, YRB_boundary)
+# 
+# elevation <- as.data.frame(elevation_crop, xy = T) %>%
+#   as_tibble() %>%
+#   rename("long" = x,
+#          "lat" = y,
+#          "elevation" = 3) %>% #column index > name (changing resolution changes colname)
+#   filter(!is.na(elevation))
 
-elevation_crop <- mask(elevation_raw, YRB_boundary)
+# ============================ read in land cover tif file ========================
 
-elevation <- as.data.frame(elevation_crop, xy = T) %>%
-  as_tibble() %>%
-  rename("long" = x,
-         "lat" = y,
-         "elevation" = 3) %>% #column index > name (changing resolution changes colname)
-  filter(!is.na(elevation))
+land_cover_tif <- list.files(land_cover_shp_dir, '\\.tif$', full.names = T)
 
-# ============================ read in cluster shp file ========================
+land_cover_raster <- rast(land_cover_tif) 
 
-cluster_shp <- list.files(cluster_shp_dir, 'shp', full.names = T)
-
-cluster <- read_sf(cluster_shp) %>%
-  st_transform(common_crs)
-
+land_cover <- as.data.frame(land_cover_raster, xy = TRUE)
 
 # ========================= create insert map ======================
 
@@ -107,11 +109,11 @@ insert <- ggplot() +
   labs(x = "", y = "")+
   theme_map()
 
-# ========================= create map of ER water column (elevation) ======================
+# ==================== create map of ER water column with land cover =================
 
 ER_wc_map <- ggplot()+
   geom_sf(data = YRB_boundary)+
-  geom_raster(data = elevation, aes(long, lat, fill = elevation), show.legend = F, alpha = 0.4)+
+  geom_raster(data = land_cover)+
   scale_fill_gradient(low = 'white', high = 'black')+
   geom_sf(data = YRB_flowlines, color = "royalblue", alpha = 0.6)+
   new_scale_fill()+
@@ -143,49 +145,58 @@ ggsave('./Data/Map_Layers/Archive_Intermediate_Files/SPS_ER_Water_Column_Map.pdf
        height = 5
 )
 
+# not using cluster map so commented out for now
+
+# ============================ read in cluster shp file ========================
+
+# cluster_shp <- list.files(cluster_shp_dir, 'shp', full.names = T)
+# 
+# cluster <- read_sf(cluster_shp) %>%
+#   st_transform(common_crs)
+
 # ========================= create map of ER water column (cluster) ======================
 
-ER_wc_map_cluster <- ggplot()+
-  geom_sf(data = YRB_boundary)+
-  geom_sf(data = cluster, aes(fill = as.factor(ClusterNum), color = as.factor(ClusterNum)), show.legend = T)+
-  # scale_fill_manual(values = alpha(c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'), 0.3))+
-  # scale_color_manual(values = alpha(c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'), 0.2))+
-  scale_fill_manual(values = c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'))+
-  scale_color_manual(values = c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'))+
-  geom_sf(data = YRB_flowlines, color = "royalblue")+
-  new_scale_fill()+
-  new_scale_color()+
-  # geom_sf(data = sites, aes(color = ER_wc, size = ER_wc), show.legend = T) +
-  # geom_sf(data = sites, aes(size = ER_wc), show.legend = T, shape = 18, fill = 'white', color = 'black') +
-  geom_sf(data = sites, show.legend = T, size = 3) +
-  # scale_fill_viridis(option = 'B', begin = 0.3)+
-  # scale_color_viridis(option = 'B', begin = 0.3)+
-  # scale_fill_gradient(low = 'white', high = 'black')+
-  # scale_color_gradient(low = 'white', high = 'black')+
-  # scale_size(range = c(0.1, 10), trans = 'reverse')+
-  theme_map() +
-  # labs(x = "", y = "", color = "Water Column\nRespiration\n(mg O2 L-1 day-1)") +
-  ggspatial::annotation_scale(
-    location = "br",
-    pad_x = unit(0.5, "in"),
-    bar_cols = c("black", "white")) +
-  ggspatial::annotation_north_arrow(
-    location = "tl", which_north = "true",
-    pad_x = unit(1.5, "in"),
-    # pad_y = unit(0.5, "in"),
-    style = ggspatial::north_arrow_nautical(
-      fill = c("black", "white"),
-      line_col = "grey20"))
-
-full_cluster <- ggdraw() +
-  draw_plot(ER_wc_map_cluster) +
-  draw_plot(insert, x = 0.4, y = 0.4, width = 0.3, height = 0.3)
-
-ggsave('./Data/Map_Layers/Archive_Intermediate_Files/SPS_ER_Water_Column_Map_Cluster.pdf',
-       full_cluster,
-       width = 8,
-       height = 5
-)
+# ER_wc_map_cluster <- ggplot()+
+#   geom_sf(data = YRB_boundary)+
+#   geom_sf(data = cluster, aes(fill = as.factor(ClusterNum), color = as.factor(ClusterNum)), show.legend = T)+
+#   # scale_fill_manual(values = alpha(c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'), 0.3))+
+#   # scale_color_manual(values = alpha(c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'), 0.2))+
+#   scale_fill_manual(values = c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'))+
+#   scale_color_manual(values = c('#1a9850', 'steelblue1', '#91cf60', '#8c510a', '#d9ef8b', '#f6e8c3'))+
+#   geom_sf(data = YRB_flowlines, color = "royalblue")+
+#   new_scale_fill()+
+#   new_scale_color()+
+#   # geom_sf(data = sites, aes(color = ER_wc, size = ER_wc), show.legend = T) +
+#   # geom_sf(data = sites, aes(size = ER_wc), show.legend = T, shape = 18, fill = 'white', color = 'black') +
+#   geom_sf(data = sites, show.legend = T, size = 3) +
+#   # scale_fill_viridis(option = 'B', begin = 0.3)+
+#   # scale_color_viridis(option = 'B', begin = 0.3)+
+#   # scale_fill_gradient(low = 'white', high = 'black')+
+#   # scale_color_gradient(low = 'white', high = 'black')+
+#   # scale_size(range = c(0.1, 10), trans = 'reverse')+
+#   theme_map() +
+#   # labs(x = "", y = "", color = "Water Column\nRespiration\n(mg O2 L-1 day-1)") +
+#   ggspatial::annotation_scale(
+#     location = "br",
+#     pad_x = unit(0.5, "in"),
+#     bar_cols = c("black", "white")) +
+#   ggspatial::annotation_north_arrow(
+#     location = "tl", which_north = "true",
+#     pad_x = unit(1.5, "in"),
+#     # pad_y = unit(0.5, "in"),
+#     style = ggspatial::north_arrow_nautical(
+#       fill = c("black", "white"),
+#       line_col = "grey20"))
+# 
+# full_cluster <- ggdraw() +
+#   draw_plot(ER_wc_map_cluster) +
+#   draw_plot(insert, x = 0.4, y = 0.4, width = 0.3, height = 0.3)
+# 
+# ggsave('./Data/Map_Layers/Archive_Intermediate_Files/SPS_ER_Water_Column_Map_Cluster.pdf',
+#        full_cluster,
+#        width = 8,
+#        height = 5
+# )
 
 
 
